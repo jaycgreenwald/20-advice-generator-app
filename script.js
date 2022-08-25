e499ed8ab33934eb5f864ef8c82ca70282f6db15
Jason Greenwald <34841215+jaycgreenwald@users.noreply.github.com>
2022-08-24 22:51:15 -0700

    8/24/2022 - Add JS API call

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-const API_URL = "https://api.adviceslip.com/advice";
+// fetch the data
+async function start() {
+    const response = await fetch("https://api.adviceslip.com/advice", {cache: "no-cache"})
+    const data = await response.json()
+    console.log(data)
+    displayAdvice(data.slip)
+}
 
-fetch(API_URL)
-    .then((response) => response.json())
-    .then((data) => { 
-        console.log(data);
-        updatePage(data);
-    })
-    
-function updatePage(data) {
-    const id = data.slip.id;
-    const advice = data.slip.advice;
-    console.log(id);
-    console.log(advice);
-    document.querySelector(".js-slip-number").innerHTML = id;
-    document.querySelector(".js-slip-advice").innerHTML = advice;
+start()
+
+// fetch new advice slip when button clicked
+document.querySelector(".js-btn-round").addEventListener("click", start)
+
+// display the slip id and advice
+function displayAdvice(adviceSlip) {
+    document.querySelector(".js-slip-number").innerHTML = adviceSlip.id
+    document.querySelector(".js-slip-advice").innerHTML = adviceSlip.advice
 }
